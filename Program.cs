304f5414d3c127d07147bf00740055622b2c2a17
Marcelaomp <marcelaompereira@gmail.com>
2025-09-19 22:02:21 -0300

    0 - criacao do menu

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿namespace ToolkitSigma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Toolkit Sigma ===");
+            Console.WriteLine();
+            bool mostrarMenu = true;
+            while (mostrarMenu)
+            {
+                CriarMenu();
+                Console.WriteLine();
+                string? opcao = SolicitarOpcao();
+                if (opcao == "0") break;
+                Console.WriteLine();                
+                DirecionarUsuario(opcao);
+            }
+            Console.WriteLine("Processo Finalizado.");
+        }
+
+        static void CriarMenu()
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1 - Verificar de alfabeto e cadeia (Σ = {a,b})");
+            Console.WriteLine("2 - Classificar T/I/N por JSON");
+            Console.WriteLine("3 - Verficar se termina com 'b'");
+            Console.WriteLine("4 - Avaliar proposições básicas");
+            Console.WriteLine("5 - Reconhecer Σ={a,b}: L_par_a e a b*");
+            Console.WriteLine("0 - Sair");
+        }
+
+        static string? SolicitarOpcao()
+        {
+            Console.Write("Escolha uma opção: ");
+            return Console.ReadLine();
+        }
+
+        static void DirecionarUsuario(string? inputUsuario)
+        {
+            switch (inputUsuario)
+            {
+                case "1": Item1.Verificador(); break;
+                case "2": Item2.ClassificadorJson(); break;
+                case "3": Item3.TerminaComB(); break;
+                case "4": Item4.AvaliadorProposicional(); break;
+                case "5": Item5.Reconhecedor(); break;
+                default: Console.WriteLine("Opção inválida."); break;
+            }
+        }         
+    }
+}
